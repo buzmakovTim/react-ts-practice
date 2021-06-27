f7931287faf8b653627a50bf9a02f6371f97ac5e
buzmakovTim <buzmakov1987@gmail.com>
2021-06-26 17:44:32 -0700

    useEffect

--- react-ts-practice/src/stories/UseEffect.stories.tsx
+++ react-ts-practice/src/stories/UseEffect.stories.tsx
@@ -1,5 +1,5 @@
 import { count } from 'node:console'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
 export default {
     title: 'useEffect demo',
@@ -45,7 +45,7 @@ export default {
 // }
 
 //Another example
-export const Clock = () => {
+export const SetInterval = () => {
 
     //console.log('Example1')
 
@@ -53,21 +53,26 @@ export const Clock = () => {
     const[minute, setMinute] = useState(0)
     const[second, setSecond] = useState(0)
 
+    // 05:05:05  time format function
     const formater = (num: number) => {
-
         return num <= 9 ? '0'+num : num
     }
 
     useEffect( () => {
-        setInterval( () => {
-            let time = new Date()        
+        const intervalId = setInterval( () => {
+            let time = new Date();        
 
-            setHour(state => state = Number(time.getHours() % 12 || 12)) // % 12 || 12  for 12 hours. without it it will be 24 hours.
-            setMinute(state => state = Number(time.getMinutes()))
-            setSecond(state => state = Number(time.getSeconds()))
+            setHour(state => state = Number(time.getHours() % 12 || 12)); // % 12 || 12  for 12 hours. without it it will be 24 hours.
+            setMinute(state => state = Number(time.getMinutes()));
+            setSecond(state => state = Number(time.getSeconds()));
 
-        }, 1000)
-    }, [])
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+
+    }, []);
     
     
 
@@ -80,4 +85,95 @@ export const Clock = () => {
         
     </>
 
+}
+
+//
+//
+//
+// useEffect Another example
+
+export const ResetEffectExample = () => {
+
+    const [counter, setCounter] = useState(1);
+
+    console.log('Conmopent rendered');
+
+    const increaseCounter = useCallback(() => {setCounter(counter + 1)}, [counter])
+
+    useEffect( () => {
+        console.log('Effect occurred!: ' + counter );
+
+        return () => {
+            console.log('Component died')
+        }
+    }, []);
+
+
+    return <>
+        Hello, counter: {counter} <button onClick={increaseCounter}>+</button>
+    </>
+
+}
+//
+//
+//
+
+
+//
+//
+//
+// useEffect Example with event listener  (on key press)
+
+export const KeyPressedListener = () => {
+
+    const[text, setText] = useState('');
+
+    useEffect(() => {
+
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key);
+            setText(text + e.key)
+        };
+
+        window.addEventListener('keypress', handler);
+        
+        return () => {
+            window.removeEventListener('keypress', handler);
+        }
+    });
+
+
+    return <>
+        Text: {text}
+
+    </>
+}
+
+
+//
+//
+//
+// useEffect Example setTimeout
+
+export const SetTimeoutExample = () => {
+
+    const[text, setText] = useState('');
+
+    useEffect(() => {
+
+        const timeoutId = setTimeout( () => {
+
+            setText('Text after 3 seconds');
+        }, 3000);
+        
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    });
+
+
+    return <>
+        Text: {text}
+
+    </>
 }
